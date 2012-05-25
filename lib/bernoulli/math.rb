5c2fc809f16e653d029a2213ec21d5c89784afa2
Mikael Konutgan <mkonutgan@gmail.com>
2012-05-25 15:20:08 +0200

    handle bad input to factorial and binomial

--- lib/bernoulli/math.rb
+++ lib/bernoulli/math.rb
@@ -2,6 +2,10 @@
 module Math
 
 	def self.factorial(n)
+		
+		if n < 0 or not n.is_a? Integer
+			raise Math::DomainError, 'Numerical argument is out of domain - "factorial"'
+		end
 		if n == 0
 			1
 		else
@@ -10,6 +14,9 @@ module Math
 	end
 
 	def self.binomial(n, k)
+		if n < 0 or n < k or not (n.is_a? Integer and k.is_a? Integer)
+			raise Math::DomainError, 'Numerical argument is out of domain - "binomial"'
+		end
 		factorial(n)/(factorial(k) * factorial(n - k))
 	end
 
